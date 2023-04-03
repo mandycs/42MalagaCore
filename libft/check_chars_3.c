781a850b4cf254481a69eaf6ddfa81efeee444aa
Mandy <mandycortessimon@gmail.com>
2023-04-03 22:34:06 +0100

    Otra ronda de fucniones de char

--- libft/check_chars_3.c
+++ libft/check_chars_3.c
@@ -23,3 +23,35 @@ unsigned char	ft_toupper(int c)
 	else
 		return (c);
 }
+
+int	ft_atoi(char *str)
+{
+	int		c;
+	long	nb;
+	int		min;
+
+	c = 0;
+	nb = 0;
+	min = 0;
+	while (str[c] == 32 || (str[c] >= 9 && str[c] <= 13))
+		c++;
+	if (str[c] == '-')
+	{
+		min = 1;
+		c++;
+	}
+	else if (str[c] == '+')
+		c++;
+	while (str[c] >= '0' && str[c] <= '9')
+	{
+		nb = nb * 10 + (str[c] - 48);
+		c++;
+	}
+	if (min == 1)
+		nb = nb * -1;
+	return (nb);
+}
+
+double	ft_atof(char *str)
+{
+}
