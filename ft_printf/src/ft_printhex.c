0622079867df0b781c182cc073c48ceb82e99f66
mandycs <mandycortessimon@gmail.com>
2023-05-29 21:35:23 +0200

    checkpoint

--- ft_printf/src/ft_printhex.c
+++ ft_printf/src/ft_printhex.c
@@ -6,18 +6,47 @@
 /*   By: mancorte <mancorte@student.42malaga.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/28 17:55:24 by mancorte          #+#    #+#             */
-/*   Updated: 2023/05/28 18:08:35 by mancorte         ###   ########.fr       */
+/*   Updated: 2023/05/29 20:43:31 by mancorte         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "libft.h"
 #include "ft_printf.h"
+#include "libft.h"
 
-int	ft_printhex(unsigned long long pt)
+int	ft_printhex(unsigned long long nb, char format)
 {
 	int	pl;
 
-	(void) pt;
 	pl = 0;
+	if (nb == 0)
+		return (write(1, '0', 1));
+	else 
+	{
+		pl = ft_numlenhex(nb);
+		ft_printhex(nb, format);
+	}
 	return (pl);
+}
+
+void	ft_printhex(unsigned long long nb, char format)
+{
+	if (nb >= 16)
+	{
+		ft_tohex(nb / 16);
+		ft_tohex(nb % 16);
+	}
+	else if (nb >= 9)
+		ft_putchar_fd(nb + 48, 1);
+	else
+	{
+		if (format == 'x')
+			ft_putchar_fd((nb - 10 + 'a'), 1);
+		else if (format == 'X')
+			ft_putchar_fd((nb - 10 + 'A'), 1);
+	}
+}
+
+int	ft_numlenhex(unsigned long long nb)
+{
+	
 }
