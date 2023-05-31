f1c88057f35f7bfe3ceb32cf9d6365bf4cab493f
mandycs <mandycortessimon@gmail.com>
2023-05-31 15:22:23 -0700

    toy muerta

--- libft/ft_strlcpy.c
+++ libft/ft_strlcpy.c
@@ -6,7 +6,7 @@
 /*   By: mancorte <mancorte@student.42malaga.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/06 18:05:41 by mancorte          #+#    #+#             */
-/*   Updated: 2023/05/13 21:51:44 by mancorte         ###   ########.fr       */
+/*   Updated: 2023/05/31 15:11:49 by mancorte         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,7 +20,9 @@ size_t	ft_strlcpy(char *dst, const char *src, size_t dstsize)
 	if (dstsize > 0)
 	{
 		while (++i < dstsize - 1 && src[i] != '\0')
+		{	
 			dst[i] = src[i];
+		}
 		dst[i] = '\0';
 	}
 	return (ft_strlen(src));
--- libft/ft_substr.c
+++ libft/ft_substr.c
@@ -6,7 +6,7 @@
 /*   By: mancorte <mancorte@student.42malaga.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/06 22:13:50 by mancorte          #+#    #+#             */
-/*   Updated: 2023/05/08 14:21:23 by mancorte         ###   ########.fr       */
+/*   Updated: 2023/05/31 15:22:06 by mancorte         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,26 +15,14 @@
 char	*ft_substr(char const *s, unsigned int start, size_t len)
 {
 	char	*str;
-	size_t	i;
-	size_t	s_len;
 
-	i = 0;
-	s_len = ft_strlen(s);
-	if (len > s_len)
-		len = s_len;
-	if (start >= s_len)
-	{
-		str = (char *)malloc(1);
-		if (!str)
-			return (NULL);
-		str[i] = '\0';
-		return (str);
-	}
+	if (len > ft_strlen(s))
+		len = ft_strlen(s);
+	if (start >= ft_strlen(s))
+		return (ft_calloc(1, 1));
 	str = (char *)malloc(sizeof(char) * (len + 1));
 	if (!s || !str)
 		return (NULL);
-	while (i < start)
-		i++;
-	ft_strlcpy(str, &s[i], len + 1);
+	ft_strlcpy(str, &s[start], len + 1);
 	return (str);
 }
