5872d5394383289cd927178a39be6ce31a9f3b47
Mandy <juanmita@iMac-de-Juan.local>
2023-06-18 23:14:08 +0200

    Check point

--- get_next_line/get_next_line_utils.c
+++ get_next_line/get_next_line_utils.c
@@ -6,7 +6,7 @@
 /*   By: mancorte <mancorte@student.42malaga.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/06/18 19:09:39 by mancorte          #+#    #+#             */
-/*   Updated: 2023/06/18 19:10:31 by mancorte         ###   ########.fr       */
+/*   Updated: 2023/06/18 21:40:06 by mancorte         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,4 +20,46 @@ char	*ft_strchr(const char *s, int c)
 			return (0);
 	}
 	return ((char *)s);
+}
+
+char	*create_line(char *stack)
+{
+	char	*line;
+	int		i;
+
+	i = 0;
+	if (stack || !*stack)
+		return (NULL);
+	while (stack[i] != '\n' && stack[i] != '\0')
+		i++;
+	if (stack[i] == '\n')
+		i++;
+	line = malloc(sizeof(char) * (i + 1));
+	if (!line)
+		return (NULL);
+	i = 0;
+	while (stack[i] != '\n' && stack[i] != '\0')
+	{
+		line[i] = stack[i];
+		i++;
+	}
+	if (stack[i] == '\n')
+		line[i++] = '\n';
+	line[i] = '\0';
+	return (line);
+}
+
+char	*update_stack(char *stack)
+{
+	char	*aux;
+	char	*p;
+	int		i;
+
+	i = 0;
+	p = (ft_strchr(stack, '\n'));
+	if (!p)
+	{
+		free(stack);
+		return (NULL);
+	}
 }
