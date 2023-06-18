ddc110d3bb34e96d4b792e40d06a9668877ecc49
Mandy <juanmita@iMac-de-Juan.local>
2023-06-18 13:59:17 +0200

    Limpieza libft

--- libft/ft_itoa.c
+++ libft/ft_itoa.c
@@ -6,11 +6,31 @@
 /*   By: mancorte <mancorte@student.42malaga.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/07 23:39:16 by mancorte          #+#    #+#             */
-/*   Updated: 2023/05/08 01:50:05 by mancorte         ###   ########.fr       */
+/*   Updated: 2023/06/18 13:55:59 by mancorte         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
+
+char	*ft_invert(char *s1)
+{
+	size_t	i;
+	size_t	j;
+	char	temp;
+
+	i = 0;
+	j = (ft_strlen(s1) - 1);
+	temp = '0';
+	while (i < (ft_strlen(s1)) / 2)
+	{
+		temp = s1[i];
+		s1[i] = s1[j];
+		s1[j] = temp;
+		i++;
+		j--;
+	}
+	return (s1);
+}
 
 char	*ft_itoa(int n)
 {
--- libft/ft_split.c
+++ libft/ft_split.c
@@ -6,11 +6,56 @@
 /*   By: mancorte <mancorte@student.42malaga.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/08 12:16:44 by mancorte          #+#    #+#             */
-/*   Updated: 2023/05/31 14:33:00 by mancorte         ###   ########.fr       */
+/*   Updated: 2023/06/18 13:57:03 by mancorte         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
+
+int	ft_count_words(const char *s, char c)
+{
+	int	i;
+	int	count;
+
+	i = 0;
+	count = 0;
+	while (s[i] != '\0')
+	{
+		if (s[i] != c)
+		{
+			count++;
+			while (s[i] != c && s[i] != '\0')
+				i++;
+		}
+		else
+			i++;
+	}
+	return (count);
+}
+
+char	**ft_fill_array(char **strs, char const *s, char c)
+{
+	size_t	i;
+	size_t	j;
+	size_t	k;
+
+	i = 0;
+	j = 0;
+	k = 0;
+	while (s[i] != '\0')
+	{
+		while (s[i] != '\0' && s[i] == c)
+			i++;
+		j = i;
+		while (s[i] != '\0' && s[i] != c)
+			i++;
+		if (j >= ft_strlen(s))
+			strs[k++] = NULL;
+		else
+			strs[k++] = ft_substr(s, j, i - j);
+	}
+	return (strs);
+}
 
 char	**ft_split(char const *s, char c)
 {
