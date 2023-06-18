83daa8cf7db7fd281895a415e3dd3af51aca93b0
mandycs <mandycortessimon@gmail.com>
2023-06-18 18:35:07 +0200

    Actualización

---
+++ libft/ft_lstiter_bonus.c
@@ -0,0 +1,24 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_lstiter.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mancorte <mancorte@student.42malaga.com    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/05/08 21:09:42 by mancorte          #+#    #+#             */
+/*   Updated: 2023/05/08 21:17:01 by mancorte         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+void	ft_lstiter(t_list *lst, void (*f)(void *))
+{
+	if (!lst)
+		return ;
+	while (lst != NULL)
+	{
+		f(lst->content);
+		lst = lst->next;
+	}
+}
--- libft/libft.h
+++ libft/libft.h
@@ -6,7 +6,7 @@
 /*   By: mancorte <mancorte@student.42malaga.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/04/22 23:40:24 by mancorte          #+#    #+#             */
-/*   Updated: 2023/05/10 22:26:07 by mancorte         ###   ########.fr       */
+/*   Updated: 2023/06/18 15:59:32 by mancorte         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -27,33 +27,19 @@ int					ft_isdigit(int c);
 int					ft_memcmp(const void *s1, const void *s2, size_t n);
 void				*ft_memset(void *b, int c, size_t len);
 char				*ft_strchr(const char *s, int c);
-int					ft_strcmp(const char *s1, const char *s2);
-char				*ft_strcpy(char *dst, const char *src);
-size_t				ft_strlen_2(const char **s);
 size_t				ft_strlen(const char *s);
 int					ft_strncmp(const char *s1, const char *s2, size_t n);
-char				*ft_strncpy(char *dst, const char *src, size_t len);
-int					ft_strnrcmp(const char *s1, const char *s2, size_t n);
 char				*ft_strnstr(const char *haystack, const char *needle,
 						size_t len);
 char				*ft_strrchr(const char *s, int c);
 void				*ft_memchr(const void *s, int c, size_t n);
 void				*ft_memcpy(void *dest, const void *src, size_t n);
-char				*ft_strstr(const char *haystack, const char *needle);
 int					ft_isprint(int c);
 int					ft_isalnum(int c);
 int					ft_isascii(int c);
-int					ft_isxdigit(int c);
-int					ft_isgraph(int c);
-int					ft_isspace(int c);
-int					ft_isblank(int c);
-int					ft_ispunct(int c);
-int					ft_iscntrl(int c);
 int					ft_tolower(int c);
 int					ft_toupper(int c);
 int					ft_atoi(const char *str);
-double				ft_sqrt(double x);
-double				ft_double(double x, double y);
 size_t				ft_strlcat(char *dst, const char *src, size_t dstsize);
 size_t				ft_strlcpy(char *dst, const char *src, size_t dstsize);
 void				*ft_calloc(size_t count, size_t size);
@@ -71,7 +57,6 @@ void				ft_putstr_fd(char *s, int fd);
 void				ft_putendl_fd(char *s, int fd);
 void				ft_putnbr_fd(int n, int fd);
 char				**ft_split(char const *s, char c);
-int					ft_indexchar(const char *s, char c);
 int					ft_count_words(const char *s, char c);
 char				**ft_fill_array(char **str, char const *s, char c);
 t_list				*ft_lstnew(void *content);
