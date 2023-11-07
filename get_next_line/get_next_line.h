2d0bc61fafa0469e8df3dbe89fb124dc913ce803
mandycs <mandycortessimon@gmail.com>
2023-11-07 22:58:56 +0100

    Pos ya

---
+++ get_next_line/get_next_line.h
@@ -0,0 +1,30 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.h                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mancorte <mancorte@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/11/06 20:23:56 by mancorte          #+#    #+#             */
+/*   Updated: 2023/11/07 18:38:10 by mancorte         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef GET_NEXT_LINE_H
+# define GET_NEXT_LINE_H
+
+# ifndef BUFFER_SIZE
+#  define BUFFER_SIZE 1
+# endif
+
+# include <stdlib.h>
+# include <unistd.h>
+
+char	*get_next_line(int fd);
+char	*ft_strchr(const char *s, int c);
+char	*ft_join_and_free(char *s1, char *s2);
+int		ft_strlen(const char *s);
+char	*ft_strdup(const char *s1);
+char	*create_line(char *stack);
+char	*update_stack(char *stack);
+#endif
