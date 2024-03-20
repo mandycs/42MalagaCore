90f0ae53bdcb12b7ad59098940573a7b15d8bbc3
mandycs <mandycortessimon@gmail.com>
2024-03-20 01:07:07 +0100

    Metodos implementados

---
+++ push_swap/push_swap.c
@@ -0,0 +1,59 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   push_swap.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: marvin <marvin@student.42.fr>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/03/19 00:04:36 by mancorte          #+#    #+#             */
+/*   Updated: 2024/03/20 00:42:34 by marvin           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+
+#include "push_swap.h"
+
+int main(int argc, char **argv)
+{
+	t_push_swap	*ps = malloc(sizeof(t_push_swap));
+	ps->stack_a = ft_init_stack();
+	ps->stack_b = ft_init_stack();
+	if (argc < 2)
+		return (1);
+	if (!ps->stack_a || !ps->stack_b)
+		return (1);
+	if (ft_prepare_stack(ps, argc, argv) == 1)
+		return (1);
+	ft_prepare_stack_b(ps);
+	if (ps->stack_b->size > 3)
+		ft_prepare_stack_b(ps);
+	return (0);
+}
+
+t_stack *ft_init_stack(void)
+{
+	t_stack *stack;
+
+	stack = (t_stack *)malloc(sizeof(t_stack));
+	if (!stack)
+		return (NULL);
+	stack->head = NULL;
+	stack->size = 0;
+	return (stack);
+}
+
+int	ft_prepare_stack(t_push_swap *ps, int argc, char **argv)
+{
+	ps->i_args = 1;
+	while (ps->i_args < argc)
+	{
+		if (ft_isdigit(argv[ps->i_args]))
+		{
+			ft_push(ps->stack_b, ft_atoi(argv[ps->i_args]));
+			ps->i_args++;
+		}
+		else
+			return (1);
+	}
+	return (0);
+}
---
+++ push_swap/push_swap.h
@@ -0,0 +1,56 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   push_swap.h                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: marvin <marvin@student.42.fr>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/03/19 00:04:49 by mancorte          #+#    #+#             */
+/*   Updated: 2024/03/19 13:52:58 by marvin           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef PUSH_SWAP_H
+# define PUSH_SWAP_H
+
+# include <stdlib.h>
+
+typedef struct s_node
+{
+    int				value;
+    struct s_node	*next;
+}				t_node;
+
+typedef struct s_stack
+{
+    t_node	*head;
+    int		size;
+}				t_stack;
+
+typedef struct s_push_swap
+{
+    int        i_args;
+    t_stack    *stack_a;
+    t_stack    *stack_b;
+}				t_push_swap;
+
+
+int main(int argc, char **argv);
+t_stack *ft_init_stack(void);
+int		ft_prepare_stack(t_stack *stack, int argc, char **argv);
+int		ft_atoi(char *str);
+int		ft_isdigit(char *str);
+int		ft_push(t_stack *stack, int value);
+void	ft_swap_a(t_stack *stack_a);
+void	ft_swap_b(t_stack *stack_b);
+void	ft_swap_ss(t_stack *stack_a, t_stack *stack_b);
+void	ft_push_a(t_stack *stack_a, t_stack *stack_b);
+void	ft_push_b(t_stack *stack_a, t_stack *stack_b);
+void	ft_rotate_a(t_stack *stack_a);
+void	ft_rotate_b(t_stack *stack_b);
+void	ft_rotate_rr(t_stack *stack_a, t_stack *stack_b);
+void	ft_reverse_rotate_a(t_stack *stack_a);
+void	ft_reverse_rotate_b(t_stack *stack_b);
+
+
+#endif
