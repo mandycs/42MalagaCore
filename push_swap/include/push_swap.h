9e6b66b72207d07a46de41e27fe34d4b53b2fe65
mandycs <mandycortessimon@gmail.com>
2024-04-10 16:39:12 +0200

    Refactor ft_split function to use char* instead of char const* in push_swap_utils.c

--- push_swap/include/push_swap.h
+++ push_swap/include/push_swap.h
@@ -55,12 +55,11 @@ void	ft_reverse_rotate_a(t_stack *stack_a);
 void	ft_reverse_rotate_b(t_stack *stack_b);
 int 	ft_prepare_stack_2(t_push_swap *ps, char **argv);
 char	**ft_split(char *s, char c, t_push_swap *ps);
-int	ft_count_words(char *s, char c);
-int		ft_strlen(char *str);
+int		ft_count_words(char *s, char c);
 char	**ft_fill_array(char **strs, char *s, char c);
-char	*ft_substr(char const *s, unsigned int start, size_t len);
 size_t	ft_strlcpy(char *dst, const char *src, size_t dstsize);
-int	ft_check_doubles(t_push_swap *ps);
-
+int		ft_check_doubles(t_push_swap *ps);
+char	*ft_substr(char const *s, unsigned int start, size_t len);
+int		ft_strlen(char *str);
 
 #endif
